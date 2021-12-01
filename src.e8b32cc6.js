81aa3a2939f2f54c5617a1d16d22579af13a634c
mpaukov <82101831+mpaukov@users.noreply.github.com>
2021-12-01 09:08:35 +0000

    Deploying to gh-pages from @ mpaukov/team-project-icecream@3d84eb329d988809cf9263afe18c1650bd004664 🚀

--- src.e8b32cc6.js
+++ src.e8b32cc6.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-icecream/src.ae853839.js.map
+//# sourceMappingURL=/team-project-icecream/src.e8b32cc6.js.map
